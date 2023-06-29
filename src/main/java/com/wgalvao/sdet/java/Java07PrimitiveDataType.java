c570775872ccbb00f2e8296a8091f222766574cd
Wagner Galvão <galvao.wagner@outlook.com.br>
2023-06-28 21:36:47 -0300

    Java Aula 07 Tipos de Dados Primitivos

---
+++ src/main/java/com/wgalvao/sdet/java/Java07PrimitiveDataType.java
@@ -0,0 +1,71 @@
+package com.wgalvao.sdet.java;
+
+/**
+ * Primitive Data Type
+ * numerical primitives
+ *         int integerMinValue = -2147483648;
+ *         int integerMaxValue = 2147483647;
+ *         short shortMinValue = -32768;
+ *         short shortMaxValue = 32767;
+ *         long longMinValue = -922337203685477580L;
+ *         long longMaxValue = 9223372036854775807L;
+ *         float floatMinValue = 1.4E-45f;
+ *         float floatMaxValue = 3.4028235E38f;
+ * others primitives
+ *         char character = 'a';
+ *         boolean booleanTrue = true;
+ *         boolean booleanFalse = false;
+ *         byte byteMinValue = -128;
+ *         byte byteMaxValue = 127;
+ * 
+ */
+public class Java07PrimitiveDataType {
+    public static void main(String[] args) {
+
+        int integerMinValue = -2147483648;
+        System.out.println("int integerMinValue = " + integerMinValue);
+
+        int integerMaxValue = 2147483647;
+        System.out.println("int integerMaxValue = " + integerMaxValue);
+
+        short shortMinValue = -32768;
+        System.out.println("short shortMinValue = " + shortMinValue);
+
+        short shortMaxValue = 32767;
+        System.out.println("short shortMaxValue = " + shortMaxValue);
+
+        long longMinValue = -922337203685477580L;
+        System.out.println("long longtMinValue = " + longMinValue);
+
+        long longMaxValue = 9223372036854775807L;
+        System.out.println("long longMaxValue = " + longMaxValue);
+
+        double doubleMinValue = 4.9E-324;
+        System.out.println("double doubletMinValue = " + doubleMinValue);
+
+        double doubleMaxValue = 1.7976931348623157E308;
+        System.out.println("double doubleMaxValue = " + doubleMaxValue);
+
+        float floatMinValue = 1.4E-45f;
+        System.out.println("float floattMinValue = " + floatMinValue);
+
+        float floatMaxValue = 3.4028235E38f;
+        System.out.println("float floatMaxValue = " + floatMaxValue);
+
+        char character = 'a';
+        System.out.println("char character = " + character);
+
+        boolean booleanTrue = true;
+        System.out.println("boolean booleanTrue = " + booleanTrue);
+
+        boolean booleanFalse = false;
+        System.out.println("boolean booleanFalse = " + booleanFalse);
+
+        byte byteMinValue = -128;
+        System.out.println("byte byteMinValue = " + byteMinValue);
+
+        byte byteMaxValue = 127;
+        System.out.println("byte byteMaxValue = " + byteMaxValue);
+
+    }
+}

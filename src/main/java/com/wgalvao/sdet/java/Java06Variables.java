a4337624270d61f4454993261613c65e65518d2d
Wagner Galvão <galvao.wagner@outlook.com.br>
2023-06-28 21:33:57 -0300

    Java Aula 06 Variáveis

---
+++ src/main/java/com/wgalvao/sdet/java/Java06Variables.java
@@ -0,0 +1,23 @@
+package com.wgalvao.sdet.java;
+
+/**
+ * Variaveis
+ * String helloWord = "Hello World!";
+ * int integerNumber = 24;
+ * double doubleNumber = 2.4;
+ * 
+ */
+public class Java06Variables {
+    public static void main(String[] args) {
+
+        String helloWord = "Hello World!";
+        System.out.println("String helloWord = " + helloWord);
+
+        int integerNumber = 24;
+        System.out.println("int integerNumber = " + integerNumber);
+
+        double doubleNumber = 2.4;
+        System.out.println("double doubleNumber = " + doubleNumber);
+
+    }
+}

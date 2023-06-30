fb4a1eef6e0e9212228159e62ff308b9d544e7f3
Wagner Galvão <galvao.wagner@outlook.com.br>
2023-06-30 12:23:21 -0300

    Java Aula 10 Entendendo os operadores bit a bit

--- src/main/java/com/wgalvao/sdet/java/Java10Operators.java
+++ src/main/java/com/wgalvao/sdet/java/Java10Operators.java
@@ -16,6 +16,7 @@ public class Java10Operators {
 
         int left = 5;
         int right = 6;
+        int negative = -4;
 
         System.out.println("Arithmetic Operators");
         System.out.println("left = " + left + " right = " + right);
@@ -45,13 +46,43 @@ public class Java10Operators {
 
         System.out.println("Bitwise Operators");
         System.out.println("left = " + left + " right = " + right);
-        System.out.println("Bitwise AND ........: left & right   " + (left & right));
-        System.out.println("Bitwise OR .........: left | right   " + (left | right));
-        System.out.println("Bitwise Exclusive OR: left ^ right   " + (left ^ right));
-        System.out.println("Bitwise Complement .: ~left          " + (~left));
-        System.out.println("Bitwise Left Shift .: right << 1     " + (right << 1));
-        System.out.println("Bitwise Right Shift : right >> 1     " + (right >> 1));
-        System.out.println("Right Zero Fill ....: left >>> right " + (left >>> right));
+        System.out.println("left " + Integer.toBinaryString(left) + " rigth "
+                + Integer.toBinaryString(right));
+        System.out.println("Bitwise AND ........: left & right   " + (left & right) + " -> "
+                + Integer.toBinaryString((left & right)));
+        System.out.println("O operador & executa uma operação AND bit a bit");
+        System.out.println("Bitwise OR .........: left | right   " + (left | right) + " -> "
+                + Integer.toBinaryString((left | right)));
+        System.out.println("O operador | executa uma operação OR inclusiva bit a bit");
+        System.out.println("Bitwise Exclusive OR: left ^ right   " + (left ^ right) + " -> "
+                + Integer.toBinaryString((left ^ right)));
+        System.out.println("O operador ^ executa uma operação OR exclusiva bit a bit");
+        System.out.println("Bitwise Complement .: ~left          " + (~left) + " -> "
+                + Integer.toBinaryString((~left)));
+        System.out.println("O operador ~ inverte cada bit do número " + left + " -> " + Integer.toBinaryString(left));
+
+        System.out.println("Bitwise Left Shift .: right << 1     " + (right << 1) + " -> "
+                + Integer.toBinaryString((right << 1)));
+        System.out.println("O operador << desloca o(s) bit(s) para a esquerda acresecentando 0 à direita do número  "
+                + right + " -> " + Integer.toBinaryString(right));
+        System.out.println("Bitwise Right Shift : right >> 1     " + (right >> 1) + " -> "
+                + Integer.toBinaryString((right >> 1)));
+        System.out.println(
+                "O operador >> desloca o(s) bit(s) para a direita acresecentando o bit do sinal à esquerda "
+                        + right + " -> " + Integer.toBinaryString(right));
+        System.out.println("Bitwise Right Shift : negative >> 1     " + (negative >> 1) + " -> "
+                + Integer.toBinaryString((negative >> 1)));
+        System.out.println(
+                "O operador >> desloca o(s) bit(s) para a direita acresecentando o bit do sinal à esquerda "
+                        + negative + " -> " + Integer.toBinaryString(negative));
+        System.out.println("Right Zero Fill ....: right >>> left " + (right >>> left) + " -> " + Integer.toBinaryString((right >>> left)));
+        System.out.println(
+                "O operador >>> desloca o(s) bit(s) para a direita sem sinal (0) à esquerda "
+                        + right + " -> " + Integer.toBinaryString(right));
+        System.out.println("Right Zero Fill ....: negative >>> left " + (negative >>> left) + " -> " + Integer.toBinaryString((negative >>> left)));
+        System.out.println(
+                "O operador >>> desloca o(s) bit(s) para a direita sem sinal (0) à esquerda "
+                        + negative + " -> " + Integer.toBinaryString(negative));
 
     }
 }

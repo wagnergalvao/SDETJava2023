e1510cbb90c0753da0d351bebfcbfe99babdf6be
Wagner Galvão <galvao.wagner@outlook.com.br>
2023-06-29 22:00:21 -0300

    Java Aula 09 Matrizes

---
+++ src/main/java/com/wgalvao/sdet/java/Java09Arrays.java
@@ -0,0 +1,21 @@
+package com.wgalvao.sdet.java;
+
+/**
+ * 
+ * Arrays
+ *         int[] myIntArray = {1,2,3};
+ * 
+ */
+public class Java09Arrays {
+    public static void main(String[] args) {
+
+        int[] myIntArray = {1,2,3};
+        System.out.println("myIntArray.length " + myIntArray.length);
+        System.out.println("myIntArray.toString() " + myIntArray.toString());
+        System.out.println("myIntArray.hashCode() " + myIntArray.hashCode());
+        System.out.println("myIntArray[0] " + myIntArray[0]);
+        System.out.println("myIntArray[1] " + myIntArray[1]);
+        System.out.println("myIntArray[2] " + myIntArray[2]);
+
+    }
+}

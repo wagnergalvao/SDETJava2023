c90de3c86b7b9d19aafbb45709511b5dd7dc7459
wagnergalvao <galvao.wagner@outlook.com.br>
2023-09-02 11:10:49 -0300

    Java Aula 14 Strings add samples

--- src/main/java/com/wgalvao/sdet/java/Java14Strings.java
+++ src/main/java/com/wgalvao/sdet/java/Java14Strings.java
@@ -6,12 +6,13 @@ package com.wgalvao.sdet.java;
  * 
  * String text02 = new String();
  * text02 = "World";
- * 
  * String text03 = "1"
  * 
- * Concat   -> text01.concat(text02.concat(text03))
- * Tamanho  -> text01.length()
+ * Concat -> text01.concat(text02.concat(text03))
+ * Tamanho -> text01.length()
  * Contains -> text01.contains("Hello")
+ * toString -> number01.toString()
+ * replace -> number01.toString().replace(".", ",")
  * 
  */
 
@@ -28,13 +29,18 @@ public class Java14Strings {
         String text03 = "!";
         System.out.println("text03 = " + text03);
 
-        System.out.println("Concatenar text01.concat(text02.concat(text03)) resulta " + text01.concat(text02.concat(text03)));
+        Float number01 = 123.456f;
+        System.out.println("number01 = " + number01);
+
+        System.out.println(
+                "text01.concat(text02.concat(text03)) resulta " + text01.concat(text02.concat(text03)));
         System.out.println("text01.length() = " + text01.length());
         System.out.println("text02.length() = " + text02.length());
         System.out.println("text03.length() = " + text03.length());
         System.out.println(text01 + " contains Hello retorna " + text01.contains("Hello"));
         System.out.println(text02 + " contains ! retorna " + text02.contains("!"));
         System.out.println(text03 + " not contains World retorna " + !text03.contains("World"));
-
+        System.out.println("number01.toString() = " + number01.toString());
+        System.out.println("number01.toString().replace(\".\", \",\") = " + number01.toString().replace(".", ","));
     }
 }

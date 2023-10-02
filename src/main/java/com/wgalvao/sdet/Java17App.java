3deb7bc1f6df004e0a21090f67bb1883c7bfa40d
wagnergalvao <galvao.wagner@outlook.com.br>
2023-10-02 18:06:10 -0300

    Java Aula 17 Objects and Classes

---
+++ src/main/java/com/wgalvao/sdet/Java17App.java
@@ -0,0 +1,15 @@
+package com.wgalvao.sdet;
+
+public class Java17App {
+    public static void main(String[] args) {
+        Java17Computer computer1 = new Java17Computer();
+        computer1.GPU = "Nvidia";
+        computer1.HARDDISK = "Seagate";
+        computer1.MOTHERBOARD = "ASUS";
+        computer1.RAM = 2048;
+        System.out.println("GPU         " + computer1.GPU);
+        System.out.println("Harddisk    " + computer1.HARDDISK);
+        System.out.println("Motherboard " + computer1.MOTHERBOARD);
+        System.out.println("RAM         " + computer1.RAM);
+    }
+}
---
+++ src/main/java/com/wgalvao/sdet/Java17Computer.java
@@ -0,0 +1,8 @@
+package com.wgalvao.sdet;
+
+public class Java17Computer {
+    int RAM;
+    String GPU;
+    String HARDDISK;
+    String MOTHERBOARD;
+}

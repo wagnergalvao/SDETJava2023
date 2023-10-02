1a378fbb946a6191cfa4a88cdbd01e4aaada5bd0
wagnergalvao <galvao.wagner@outlook.com.br>
2023-10-02 17:27:32 -0300

    Java Aula 16 Switch Case Statements

---
+++ src/main/java/com/wgalvao/sdet/java/Java16SwitchCaseStatement.java
@@ -0,0 +1,45 @@
+package com.wgalvao.sdet.java;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+
+/**
+ * Scanner scanner = new Scanner(System.in);
+ * 
+ * String sentence = scanner.nextLine();
+ * 
+ * Integer number = scanner.nextInt();
+ * 
+ */
+
+public class Java16SwitchCaseStatement {
+    public static void main(String[] args) {
+
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("today " + now);
+
+        switch (now.getDayOfWeek().getValue()) {
+            case 1:
+                System.out.println("Segunda-feira, " + now);
+                break;
+            case 2:
+                System.out.println("Terça-feira, " + now);
+                break;
+            case 3:
+                System.out.println("Quarta-feira, " + now);
+                break;
+            case 4:
+                System.out.println("Quinta-feira, " + now);
+                break;
+            case 5:
+                System.out.println("Sexta-feira, " + now);
+                break;
+            case 6:
+                System.out.println("Sábado, " + now);
+                break;
+            case 7:
+                System.out.println("Domingo-feira, " + now);
+                break;
+        }
+    }
+}

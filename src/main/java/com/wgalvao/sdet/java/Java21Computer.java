595ed7df71b33271809bb0c5dfed53d801f8d00e
wagnergalvao <galvao.wagner@outlook.com.br>
2023-10-02 22:58:59 -0300

    Java Aula 21 Getters and Setters

---
+++ src/main/java/com/wgalvao/sdet/java/Java21Computer.java
@@ -0,0 +1,55 @@
+package com.wgalvao.sdet.java;
+
+public class Java21Computer {
+    int ram;
+    String gpu;
+    String harddisk;
+    String motherboard;
+    String processor;
+
+    public Java21Computer() {
+        System.out.println("Carregado o construtor Java21Computer");
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public String getGpu() {
+        return gpu;
+    }
+
+    public void setGpu(String gpu) {
+        this.gpu = gpu;
+    }
+
+    public String getHarddisk() {
+        return harddisk;
+    }
+
+    public void setHarddisk(String harddisk) {
+        this.harddisk = harddisk;
+    }
+
+    public String getMotherboard() {
+        return motherboard;
+    }
+
+    public void setMotherboard(String motherboard) {
+        this.motherboard = motherboard;
+    }
+
+    public String getProcessor() {
+        return processor;
+    }
+
+    public void setProcessor(String processor) {
+        this.processor = processor;
+    }
+
+
+}
